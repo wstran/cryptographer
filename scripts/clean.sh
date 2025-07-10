70aa590b23ef101f5b5df1993cb1bace0535bad1
Wilson Tran <wstran@192.168.1.15>
2025-07-11 02:38:18 +0700

    update clean script for sha2 crate

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,2 +1,5 @@
-# clean up the build artifacts and directories of the Blake3 WASM crate
+# clean up the build artifacts and directories of the BLAKE3 WASM crate
 rm -rf ../../crates/blake3_wasm/target ../../crates/blake3_wasm/Cargo.lock ../../packages/blake3_wasm
+
+# clean up the build artifacts and directories of the SHA2 WASM crate
+rm -rf ../../crates/sha2_wasm/target ../../crates/sha2_wasm/Cargo.lock ../../packages/sha2_wasm
