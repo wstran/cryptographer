50968f2bc8e02d9a50a68f870b390b6e8813fc2c
Wilson Tran <wstran@Wilsons-MacBook-Pro.local>
2025-08-06 20:37:15 +0700

    fix: properly include packages directory in npm package - Add packages/ to .npmignore include list - Bump version to 1.0.7 - Update all documentation references

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const crypto = require('cryptographer.js');
 
-console.log('🔐 Testing cryptographer.js v1.0.6');
+console.log('🔐 Testing cryptographer.js v1.0.7');
 console.log('=====================================');
 
 // Test basic imports
