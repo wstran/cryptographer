a599b586ed74155e4ea4c38c8e12f51e5d549cf4
Wilson Tran <wstran@Wilsons-MacBook-Pro.local>
2025-08-06 20:25:59 +0700

    chore: bump version to 1.0.5 - Update package.json version to 1.0.5 - Update all documentation references to v1.0.5

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const crypto = require('cryptographer.js');
 
-console.log('🔐 Testing cryptographer.js v1.0.3');
+console.log('🔐 Testing cryptographer.js v1.0.5');
 console.log('=====================================');
 
 // Test basic imports
